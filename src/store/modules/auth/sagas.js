2b273891b03cab43bece5bbe0f6f7c73808818a4
Pedro Araujo <pedro-foa@hotmail.com>
2019-11-12 13:58:15 -0300

    created persited redux for login

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -6,18 +7,23 @@ import api from '~/services/api';
 import { signInSuccess, signInFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  yield put(signInSuccess(token, user));
+    yield put(signInSuccess(token, user));
 
-  history.push('/students');
+    history.push('/students');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique os seus dados!');
+    yield put(signInFailure());
+  }
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
